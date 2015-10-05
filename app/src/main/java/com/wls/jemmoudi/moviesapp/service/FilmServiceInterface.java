5878d880a90f34393605bea5df57012ac4f93b7e
Mohammed Jemmoudi <med.jemmoudi@gmail.com>
2015-10-05 02:03:56 +0100

    Update FilmServiceInterface.java
    Update ENDPOINT

--- app/src/main/java/com/wls/jemmoudi/moviesapp/service/FilmServiceInterface.java
+++ app/src/main/java/com/wls/jemmoudi/moviesapp/service/FilmServiceInterface.java
@@ -9,7 +9,7 @@ import retrofit.http.Path;
 
 public interface FilmServiceInterface {
 
-    public static final String ENDPOINT = "http://192.168.1.32:80/2wls";
+    public static final String ENDPOINT = "http://pmt.16mb.com/2wls";
 
     /**
      * it returns a collection of all movies
